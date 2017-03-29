12bb76c1496c539bcb24f46e04da64df95233dc7
fdmansilla333 <fdmansilla333@gmail.com>
2017-03-29 19:41:26 -0300

    Agregando ultimas pruebas del modulo esp 8266

--- Test/Wifi/pruebaVelocidad4-configuracionWifi/pruebaVelocidad4-configuracionWifi.ino
+++ Test/Wifi/pruebaVelocidad4-configuracionWifi/pruebaVelocidad4-configuracionWifi.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 /**
- * Configuramos el BT a 50000000 y conseguimos una velocidad de 54kb/sg
+ * Configuramos el BT a 5000000 y conseguimos una velocidad de 54kb/sg
  */
 SoftwareSerial ESP(3, 2); // RX | TX
 /*
@@ -9,18 +9,18 @@ Enviar comando al esp8266 y verificar la respuesta del módulo, todo esto dentro
 void sendData(String comando, const int timeout)
 {
  long int time = millis(); // medir el tiempo actual para verificar timeout
- 
+
  ESP.print(comando); // enviar el comando al ESP8266
- 
+
  while( (time+timeout) > millis()) //mientras no haya timeout
  {
  while(ESP.available()) //mientras haya datos por leer
- { 
+ {
  // Leer los datos disponibles
  char c = ESP.read(); // leer el siguiente caracter
  Serial.print(c);
  }
- } 
+ }
  return;
 }
 //Funcion para llenar un buffer con 1024 elementos
@@ -28,7 +28,7 @@ void armarBuffer(char buf[], int inicio, int fin){
   for(int i=inicio; i<=fin; i++){
     buf[i]='1';
   }
-  
+
 }
 char frame[1024]; //En 2048 se queda con problemas Arduino UNO, por quedarse sin espacio
 //Se configura el serial para imprimir las opciones
@@ -39,17 +39,17 @@ void setup()
      ESP.begin(5000000);
      sendData("AT+CIPMUX=1\r\n",1000); // configurar para multiples conexiones
      sendData("AT+CIPSERVER=1,80\r\n",1000);         // servidor en el puerto 80
-    
-     
+
+
      armarBuffer(frame,0,1023);
   }
 
 //Este toma de la entrada estandar un 1 para comenzar la prueba de transmitir un buffer de 1kb, en 1 sg, por medio del wifi.
 //De esta forma, se puede terminar la máxima velocidad reduciendo el timeout.
 void loop()
-  {  
+  {
    if(Serial.available()){
-      
+
       //Si esta disponible leo del buffer
       char opc = Serial.read();
       if(opc == '1'){
@@ -76,5 +76,3 @@ void loop()
       }
       }
     }
-   
-   
