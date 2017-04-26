6b94a6d3a3c7cf570c30034c5a3e34e99e138a08
fdmansilla333 <fdmansilla333@gmail.com>
2017-04-26 10:52:34 -0300

    Agregado pruebas de comuncacion a traves de serie

---
+++ Test/ComunicacionSerieDosArduinos/MasterSerie/MasterSerie.ino
@@ -0,0 +1,13 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerie(10,11); // TX, RX
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
