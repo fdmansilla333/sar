e8835b323a70e28278d40473c08cb52afe70b488
Damian <fdmansilla333@gmail.com>
2018-03-07 21:44:26 -0300

    Agregando compodoc
    Agregando compodoc

---
+++ Software/aplicacionSar/documentation/js/routes/routes_index.js
@@ -0,0 +1,1 @@
+var ROUTES_INDEX = {"name":"<root>","kind":"module","className":"AppModule","children":[{"kind":"component","component":"PizarraComponent","path":""},{"kind":"component","component":"EstadisticasComponent","path":"estadisticas"},{"kind":"component","component":"PanelInferiorComponent","path":"control"}]}
